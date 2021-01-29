d53964f994227fad119f23bd8866b17489b421f7
SungWoo-Park <park.sungwoo@navercorp.com>
2021-01-29 22:40:54 +0900

    migration

--- app/src/main/java/com/example/user/timecircle/TimeCircleFragment.kt
+++ app/src/main/java/com/example/user/timecircle/TimeCircleFragment.kt
@@ -1,45 +1,17 @@
 package com.example.user.timecircle
 
-import android.graphics.Color
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.util.Log.i
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.time_circle_fragment.*
-import org.jetbrains.anko.sdk25.coroutines.onClick
-import org.jetbrains.anko.sdk25.coroutines.onLongClick
-import org.jetbrains.anko.sdk25.coroutines.onTouch
-import org.jetbrains.anko.support.v4.dimen
-import kotlin.math.atan2
 
 /**
  * Created by SungWoo on 2018-08-06.
  */
 
-private const val INIT_ROTATION_ANGLE = 0.0f
-private const val SCALE1 = 1.0f
-private const val SCALE2 = 2.0f
-private const val CIRCLE_IMAGE_SCALE1 = 1.0f
-private const val CIRCLE_IMAGE_SCALE2 = 1.6f
-private const val position1 = 0.0f
-private const val position2 = 500.0f
-private const val duration: Long = 500
-private const val rotateBaseRightUnit = -4
-private const val rotateBaseLeftUnit = 3
-private const val MAX_ROTATE_INDEX = 3
-
 class TimeCircleFragment : Fragment() {
-    private val centerX by lazy { dimen(R.dimen.time_circle_length) / 2 }
-    private val centerY by lazy { dimen(R.dimen.time_circle_length) / 2 }
-    private var x = 0f
-    private val circleViews = arrayOfNulls<CircleView>(CIRCLE_NUM)
-    private var isSelectionMode = false
-    private var rotateAngle = INIT_ROTATION_ANGLE
-    private var isZoomed = false
-    private var rotateBaseIndex = CIRCLE_NUM / 2
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.time_circle_fragment, container, false)
@@ -47,125 +19,6 @@ class TimeCircleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        time_circle_root_layout.setOnClickListener { zoomOut() }
-        time_circle_frame_layout.onClick { zoomIn() }
-        time_circle_frame_layout.onTouch { _, event -> onTimeCircleTouched(event) }
-        time_circle_frame_layout.onLongClick { changeToSelectionMode() }
-
-        for (i in 0 until CIRCLE_NUM) {
-            val circleView = CircleView(context)
-            circleView.z = 1.0f
-            time_circle_inner_frame_layout.addView(circleView)
-            circleViews[i] = circleView
-        }
+        time_circle_root_layout.setOnClickListener { time_circle_frame_layout.zoomOut() }
     }
-
-    private fun zoomIn() {
-        if (!isZoomed) {
-            x = time_circle_frame_layout.x
-            time_circle_frame_layout.animate().scaleX(SCALE2).scaleY(SCALE2).x(-540f).y(position2).setDuration(duration).start()
-//            circleImageView.animate().scaleX(CIRCLE_IMAGE_SCALE2).scaleY(CIRCLE_IMAGE_SCALE2).setDuration(duration).start()
-            isZoomed = true
-        }
-    }
-
-    private fun zoomOut() {
-        if (isZoomed) {
-            initValues()
-            time_circle_frame_layout.animate().scaleX(SCALE1).scaleY(SCALE1).x(x).y(position1).setDuration(duration).rotation(rotateAngle).start()
-//            circleImageView.animate().scaleX(CIRCLE_IMAGE_SCALE1).scaleY(CIRCLE_IMAGE_SCALE1).setDuration(duration).start()
-            isZoomed = false
-        }
-    }
-
-    private fun initValues() {
-        rotateAngle = INIT_ROTATION_ANGLE
-        rotateBaseIndex = CIRCLE_NUM / 2
-    }
-
-    private fun changeToSelectionMode() {
-        if (isZoomed) {
-            isSelectionMode = true
-        }
-    }
-
-    private fun onTimeCircleTouched(motionEvent: MotionEvent): Boolean {
-        if (!isZoomed) {
-            return false
-        }
-
-        if (motionEvent.action == MotionEvent.ACTION_UP) {
-            isSelectionMode = false
-            return true
-        }
-
-        if (!isSelectionMode) {
-            return false
-        }
-
-        val x = centerX - motionEvent.x
-        val y = centerY - motionEvent.y
-
-        val length = square(x) + square(y)
-
-        if (isTouchedInCircle(length)) {
-            changeColorAndRotate(x, y)
-        }
-        return true
-    }
-
-    private fun isTouchedInCircle(length: Float): Boolean {
-        return length < square(dimen(R.dimen.time_circle_length) / 2) && length > square(dimen(R.dimen.time_image_length) / 2)
-    }
-
-    private fun changeColorAndRotate(x: Float, y: Float) {
-        val touchedIndex = getCircleIndex(x, y)
-        circleViews[touchedIndex]?.apply {
-            color = Color.GREEN
-            invalidate()
-        }
-
-        i("coco", "rotatebseindex $rotateBaseIndex circleindex $touchedIndex")
-        var rotateIndex = calculateRotateIndex(touchedIndex)
-        if (rotateIndex == 0) return
-
-        i("COCO", "rotationIndex $rotateIndex")
-        rotateIndex = when {
-            rotateIndex > MAX_ROTATE_INDEX -> MAX_ROTATE_INDEX
-            rotateIndex < -MAX_ROTATE_INDEX -> -MAX_ROTATE_INDEX
-            else -> rotateIndex
-        }
-        rotateBaseIndex += rotateIndex
-        if (rotateBaseIndex > CIRCLE_NUM - 1) rotateBaseIndex -= CIRCLE_NUM
-        else if (rotateBaseIndex < 0) rotateBaseIndex += CIRCLE_NUM
-        rotateAngle += rotateIndex * UNIT_ANGLE
-        time_circle_frame_layout.animate().rotation(-rotateAngle).setDuration(300).start()
-    }
-
-    private fun calculateRotateIndex(touchedIndex: Int): Int {
-        return when {
-            touchedIndex < CIRCLE_NUM / 4 && CIRCLE_NUM * 3 / 4 < rotateBaseIndex ->
-                if (CIRCLE_NUM + touchedIndex - rotateBaseIndex > rotateBaseLeftUnit) {
-                    CIRCLE_NUM + touchedIndex - rotateBaseIndex - rotateBaseLeftUnit
-                } else {
-                    0
-                }
-            rotateBaseIndex < CIRCLE_NUM / 4 && CIRCLE_NUM * 3 / 4 < touchedIndex ->
-                if (-CIRCLE_NUM + touchedIndex - rotateBaseIndex < rotateBaseRightUnit) {
-                    -CIRCLE_NUM + touchedIndex - rotateBaseIndex - rotateBaseRightUnit
-                } else {
-                    0
-                }
-            touchedIndex - rotateBaseIndex > rotateBaseLeftUnit -> touchedIndex - rotateBaseIndex - rotateBaseLeftUnit
-            touchedIndex - rotateBaseIndex < rotateBaseRightUnit -> touchedIndex - rotateBaseIndex - rotateBaseRightUnit
-            else -> 0
-        }
-    }
-
-    private fun getCircleIndex(x: Float, y: Float): Int {
-        return (((-atan2(x, y) * (180 / Math.PI) + 360) % 360) / UNIT_ANGLE).toInt()
-    }
-
-    private fun square(mono: Float): Float = mono * mono
-    private fun square(mono: Int): Float = (mono * mono).toFloat()
 }
---
+++ app/src/main/java/com/example/user/timecircle/views/TimeCircleLayout.kt
@@ -0,0 +1,171 @@
+package com.example.user.timecircle.views
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.FrameLayout
+import com.example.user.timecircle.CIRCLE_NUM
+import com.example.user.timecircle.CircleView
+import com.example.user.timecircle.R
+import com.example.user.timecircle.UNIT_ANGLE
+import kotlinx.android.synthetic.main.time_circle_fragment.view.*
+import org.jetbrains.anko.dimen
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.sdk25.coroutines.onLongClick
+import org.jetbrains.anko.sdk25.coroutines.onTouch
+import kotlin.math.atan2
+
+private const val INIT_ROTATION_ANGLE = 0.0f
+private const val SCALE1 = 1.0f
+private const val SCALE2 = 2.0f
+private const val CIRCLE_IMAGE_SCALE1 = 1.0f
+private const val CIRCLE_IMAGE_SCALE2 = 1.6f
+private const val ZOOM_OUT_Y = 0.0f
+private const val ZOOM_IN_Y = 500.0f
+private const val ZOOM_IN_X = -540.0f
+private const val DURATION: Long = 500
+private const val ROTATE_BASE_RIGHT_UNIT = -4
+private const val ROTATE_BASE_LEFT_UNIT = 3
+private const val MAX_ROTATE_INDEX = 3
+
+class TimeCircleLayout @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var isZoomed = false
+    private val centerX by lazy { getContext().dimen(R.dimen.time_circle_length) / 2 }
+    private val centerY by lazy { getContext().dimen(R.dimen.time_circle_length) / 2 }
+    private var originX = 0f
+    private val circleViews = arrayOfNulls<CircleView>(CIRCLE_NUM)
+    private var isSelectionMode = false
+    private var rotateAngle = INIT_ROTATION_ANGLE
+
+    private var rotateBaseIndex = CIRCLE_NUM / 4
+
+    override fun onViewAdded(child: View?) {
+        super.onViewAdded(child)
+        onClick { zoomIn() }
+        onTouch { _, event -> onTimeCircleTouched(event) }
+        onLongClick { changeToSelectionMode() }
+
+        for (i in 0 until CIRCLE_NUM) {
+            val circleView = CircleView(context)
+            circleView.z = 1.0f
+            time_circle_inner_frame_layout.addView(circleView)
+            circleViews[i] = circleView
+        }
+
+        initValues()
+    }
+
+    private fun zoomIn() {
+        if (!isZoomed) {
+            originX = x
+            animate().scaleX(SCALE2).scaleY(SCALE2).x(ZOOM_IN_X).y(ZOOM_IN_Y).setDuration(DURATION).start()
+//            circleImageView.animate().scaleX(CIRCLE_IMAGE_SCALE2).scaleY(CIRCLE_IMAGE_SCALE2).setDuration(duration).start()
+            isZoomed = true
+        }
+    }
+
+    fun zoomOut() {
+        if (isZoomed) {
+            initValues()
+            animate().scaleX(SCALE1).scaleY(SCALE1).x(originX).y(ZOOM_OUT_Y).setDuration(DURATION).rotation(rotateAngle).start()
+//            circleImageView.animate().scaleX(CIRCLE_IMAGE_SCALE1).scaleY(CIRCLE_IMAGE_SCALE1).setDuration(duration).start()
+            isZoomed = false
+        }
+    }
+
+    private fun initValues() {
+        rotateAngle = INIT_ROTATION_ANGLE
+        rotateBaseIndex = CIRCLE_NUM / 4
+    }
+
+    private fun changeToSelectionMode() {
+        if (isZoomed) {
+            isSelectionMode = true
+        }
+    }
+
+    private fun onTimeCircleTouched(motionEvent: MotionEvent): Boolean {
+        if (!isZoomed) {
+            return false
+        }
+
+        if (motionEvent.action == MotionEvent.ACTION_UP) {
+            isSelectionMode = false
+            return true
+        }
+
+        if (!isSelectionMode) {
+            return false
+        }
+
+        val x = centerX - motionEvent.x
+        val y = centerY - motionEvent.y
+
+        val length = square(x) + square(y)
+
+        if (isTouchedInCircle(length)) {
+            changeColorAndRotate(x, y)
+        }
+        return true
+    }
+
+    private fun isTouchedInCircle(length: Float): Boolean {
+        return length < square(dimen(R.dimen.time_circle_length) / 2) && length > square(dimen(R.dimen.time_image_length) / 2)
+    }
+
+    private fun changeColorAndRotate(x: Float, y: Float) {
+        // touchedIndex 현재 눌린 시간 인덱
+        val touchedIndex = getCircleIndex(x, y)
+        circleViews[touchedIndex]?.apply {
+            color = Color.GREEN
+            invalidate()
+        }
+
+        Log.i("coco", "rotateBaseIndex $rotateBaseIndex touchedIndex $touchedIndex")
+        var rotateIndex = calculateRotateIndex(touchedIndex)
+        //회전 시킬 일 없으면 return
+        if (rotateIndex == 0) return
+
+        Log.i("coco", "rotateIndex $rotateIndex")
+        rotateIndex = rotateIndex.coerceIn(-MAX_ROTATE_INDEX, -MAX_ROTATE_INDEX)
+
+        rotateBaseIndex += rotateIndex
+        Log.i("coco", "coerceIn $rotateIndex")
+        if (rotateBaseIndex > CIRCLE_NUM - 1) rotateBaseIndex -= CIRCLE_NUM
+        else if (rotateBaseIndex < 0) rotateBaseIndex += CIRCLE_NUM
+        rotateAngle += rotateIndex * UNIT_ANGLE
+        animate().rotation(-rotateAngle).setDuration(300).start()
+    }
+
+    // rotateBaseIndex와 몇 칸 떨어져 있는지 계산
+    private fun calculateRotateIndex(touchedIndex: Int): Int {
+        return when {
+            touchedIndex < CIRCLE_NUM / 4 && CIRCLE_NUM * 3 / 4 < rotateBaseIndex ->
+                if (CIRCLE_NUM + touchedIndex - rotateBaseIndex > ROTATE_BASE_LEFT_UNIT) {
+                    CIRCLE_NUM + touchedIndex - rotateBaseIndex - ROTATE_BASE_LEFT_UNIT
+                } else {
+                    0
+                }
+            rotateBaseIndex < CIRCLE_NUM / 4 && CIRCLE_NUM * 3 / 4 < touchedIndex ->
+                if (-CIRCLE_NUM + touchedIndex - rotateBaseIndex < ROTATE_BASE_RIGHT_UNIT) {
+                    -CIRCLE_NUM + touchedIndex - rotateBaseIndex - ROTATE_BASE_RIGHT_UNIT
+                } else {
+                    0
+                }
+            touchedIndex - rotateBaseIndex > ROTATE_BASE_LEFT_UNIT -> touchedIndex - rotateBaseIndex - ROTATE_BASE_LEFT_UNIT
+            touchedIndex - rotateBaseIndex < ROTATE_BASE_RIGHT_UNIT -> touchedIndex - rotateBaseIndex - ROTATE_BASE_RIGHT_UNIT
+            else -> 0
+        }
+    }
+
+    private fun getCircleIndex(x: Float, y: Float): Int {
+        return (((-atan2(x, y) * (180 / Math.PI) + 360) % 360) / UNIT_ANGLE).toInt()
+    }
+
+    private fun square(mono: Float): Float = mono * mono
+    private fun square(mono: Int): Float = (mono * mono).toFloat()
+}
